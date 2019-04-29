956a762fa583f5efd01767923ba11a6ae80b6fb7
Jose Tantera <jose_tantera@yahoo.com.ar>
2019-04-28 21:12:22 -0300

    modificado el server - creada la clase

--- Python/Chat/chat.py
+++ Python/Chat/chat.py
@@ -2,38 +2,61 @@ import socket
 import threading
 import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.bind(('0.0.0.0', 12000))
-sock.listen(1)
+class Server:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connections = []
+    
+    def __init__(self):
+        self.sock.bind(('0.0.0.0', 10000))
+        self.sock.listen(1)
 
-
-connections = []
-
-
-def handler(c, a):
-    global connections
-    while True:
-        data = c.recv(1024)
-        if data == startswith("exit"):
-            for connection in connections:
-                print("Cerrando conexion {c}")
+    def handler(self, c, a):
+        while True:
+            data = c.recv(1024)
+            if data == 0:
+                print("Cerrando conexiones")
+                self.closeall()
+            
+            for connection in self.connections:
+                connection.send(bytes(data))
+            if not data:
                 connection.remove(c)
                 c.close()
-                thread.exit()
+                break
+                
+    def run(self):
+        while True:
+            c, a = self.sock.accept()
+            cThread = threading.Thread(target = self.handler, args=(c,a))
+            cThread.daemon = True                            #Me permite cerrar el programa por mas que los threads esten abiertos
+            cThread.start()
+            self.connections.append(c)
+            print(self.connections)
+            
+    def closeall(self)
         for connection in connections:
-            connection.send(bytes(data))
-        if not data:
+            print("Cerrando conexion {c}")
             connection.remove(c)
             c.close()
-            break
+            thread.exit()
+     
+            
+            
+            
+server = Server()
+server.run()
 
 
 
-while True:
-    c, a = sock.accept()
-    cThread = threading.Thread(target = handler, args=(c,a))
-    cThread.daemon = True                            #Me permite cerrar el programa por mas que los threads esten abiertos
-    cThread.start()
-    connections.append(c)
-    print(connections)
+
+#if data == startswith("exit"):
+#    for connection in connections:
+#        print("Cerrando conexion {c}")
+#        connection.remove(c)
+#        c.close()
+#        thread.exit()
+            
+
+
+
