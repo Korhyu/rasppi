a7976893ff2d5fbca04f4b703f15d129e60ff2e2
Jose <jose_tantera@yahoo.com.ar>
2019-07-24 20:55:43 -0300

    Servidor Funcionando
    Estamos tratando de hacer funcionar el cliente

---
+++ C-C++/Basado_TD3/Servidor/aux.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
